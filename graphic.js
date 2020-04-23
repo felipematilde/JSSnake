e0fd1f7b7c214f737c3d36b7f877f4b587ef8ab8
FREM <58148463+felipematilde@users.noreply.github.com>
2020-04-23 18:40:19 -0300

    Refactoring code to use javascript class
    * Class square
    * Class snake
    * Other refactoring

--- graphic.js
+++ graphic.js
@@ -1,8 +1,9 @@
 //Parameters
-var snakeSize = 20;
+var snakeSize = 20; //px
 var initialX = 5, initialY = 5;
 var gameAreaWidth = 700;
 var gameAreaHeight = 700;
+var gameAreaBorder = "thin solid #0000FF";
 var snakeColor = "#008000";
 var fruitColor = "#FF0000";
 var gameAreaColor = "#ADFF2F";
@@ -11,66 +12,50 @@ var snakeIsAlive = true;
 var snakeInitialSpeed = 200; //ms
 var snakeSpeed = snakeInitialSpeed;
 
+class Square{
+    //Attributes:
+    //  div;
+    //  x;
+    //  y;
+    constructor(width, height, color, x, y, border = ""){
+        this.div = document.createElement("div");
+        this.div.className = "box";
+        this.div.style.width = width;
+        this.div.style.height = height;
+        this.div.style.backgroundColor = color;
+        this.div.style.position = "absolute";
+        this.div.style.top = y*snakeSize;
+        this.div.style.left = x*snakeSize;
+        this.div.style.border = border;
+        document.body.appendChild(this.div);
+
+        this.x = x;
+        this.y = y;
+    }
+
+    setXY(x,y){
+        this.x = x;
+        this.y = y;
+        this.div.style.top = y*snakeSize;
+        this.div.style.left = x*snakeSize;
+    }
+
+    removeDiv(){
+        document.body.removeChild(this.div);
+    }
+}
+
 //GameArea
-var gameArea = document.createElement("div");
-gameArea.className = "box";
-gameArea.style.width = gameAreaWidth;
-gameArea.style.height = gameAreaHeight;
-gameArea.style.backgroundColor = gameAreaColor;
-gameArea.style.position = "absolute";
-gameArea.style.top = 0;
-gameArea.style.left = 0;
-gameArea.style.border = "thin solid #0000FF";
-document.body.appendChild(gameArea);
+var gameArea = new Square(gameAreaWidth, gameAreaHeight, gameAreaColor, 0, 0, gameAreaBorder);
 
 //Snake body
-var head = document.createElement("div");
-head.className = "box";
-head.style.width = snakeSize;
-head.style.height = snakeSize;
-head.style.backgroundColor = snakeHeadColor;
-head.style.position = "absolute";
-head.style.top = 0;
-head.style.left = 0;
-
-var piece1 = document.createElement("div");
-piece1.className = "box";
-piece1.style.width = snakeSize;
-piece1.style.height = snakeSize;
-piece1.style.backgroundColor = snakeColor;
-piece1.style.position = "absolute";
-piece1.style.top = 0;
-piece1.style.left = 0;
-
-var piece2 = document.createElement("div");
-piece2.className = "box";
-piece2.style.width = snakeSize;
-piece2.style.height = snakeSize;
-piece2.style.backgroundColor = snakeColor;
-piece2.style.position = "absolute";
-piece2.style.top = 0;
-piece2.style.left = 0;
-
-var piece3 = document.createElement("div");
-piece3.className = "box";
-piece3.style.width = snakeSize;
-piece3.style.height = snakeSize;
-piece3.style.backgroundColor = snakeColor;
-piece3.style.position = "absolute";
-piece3.style.top = 0;
-piece3.style.left = 0;
-var snake = {direction: "down", body: [{x:initialX,y:initialY,piece:head},{x:initialX,y:initialY-1,piece:piece1},{x:initialX,y:initialY-2,piece:piece2},{x:initialX,y:initialY-3,piece:piece3}]};
+var head = new Square(snakeSize, snakeSize, snakeHeadColor, initialX, initialY);
+var piece1 = new Square(snakeSize, snakeSize, snakeColor, initialX, initialY-1);
+var piece2 = new Square(snakeSize, snakeSize, snakeColor, initialX, initialY-2);
+var piece3 = new Square(snakeSize, snakeSize, snakeColor, initialX, initialY-3);
 
 //Fruit
-var fruitPiece = document.createElement("div");
-fruitPiece.classList = "box";
-fruitPiece.style.width = snakeSize;
-fruitPiece.style.height = snakeSize;
-fruitPiece.style.backgroundColor = fruitColor;
-fruitPiece.style.position = "absolute";
-fruitPiece.style.top = 0;
-fruitPiece.style.left = 0;
-var fruit = {x: 10, y:20, piece:fruitPiece};
+var fruit = new Square(snakeSize, snakeSize, fruitColor,10,20);
 
 //Score
 var scoreDisplay = document.createElement("output");
